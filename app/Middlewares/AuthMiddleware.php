0baf8f6ad9c1c5b2b4f43de98fdadb6be73e7161
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2025-03-09 16:36:02 -0300

    feat: add dotenv and jwt libs

--- app/Middlewares/AuthMiddleware.php
+++ app/Middlewares/AuthMiddleware.php
@@ -2,21 +2,26 @@
 
 namespace App\Middlewares;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
+use UnexpectedValueException;
 
 class AuthMiddleware
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        if ($request->getHeaderLine('Authorization') !== 'nonsecure') {
+        JWT::$leeway = 10800;
+        try {
+            $decoded = JWT::decode($request->getHeaderLine('Authorization'), new Key($_ENV['SECRET_KEY'], 'HS256'));
+
+            return $handler->handle($request);
+        } catch (UnexpectedValueException $e) {
             $response = new Response();
-            $response->getBody()->write('Authorization work in progress');
-
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
+            $response->getBody()->write(json_encode(['error' => $e->getMessage()]));
+            return $response->withStatus(401);
         }
-
-        return $handler->handle($request);
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?phP
 
+date_default_timezone_set('America/Sao_Paulo');
+
 require __DIR__ . '/../vendor/autoload.php';
 require '../app/routes.php';
